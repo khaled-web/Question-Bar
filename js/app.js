0f275ca06e7f68fd5021ced93a99c8d1969768aa
Khaled Mohamed Ibrahim <eng.khaled_ok@outlook.com>
2022-02-01 14:04:13 +0200

    The full data are physically completed

---
+++ js/app.js
@@ -0,0 +1,8 @@
+const toggles = document.querySelectorAll('.faq-toggle');
+const faq = document.querySelector('.faq')
+
+toggles.forEach(toggle => {
+ toggle.addEventListener('click', () => {
+  toggle.parentNode.classList.toggle('active')
+ })
+})
